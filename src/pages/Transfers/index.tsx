1bcb25b5701d653784a5461e036e5f18a5cb8fac
Van Hohenheim <0xhohenheim@gmail.com>
2024-09-12 02:15:51 +0530

    feat: transfers/history page

--- src/pages/Transfers/index.tsx
+++ src/pages/Transfers/index.tsx
@@ -1,9 +1,33 @@
+import { useGetTransfers } from "../../hooks/useGetTransfers";
+import { getImageFromUser, shortAddress } from "../../utils";
 import styles from "./Transfers.module.scss";
 
 export default function Transfers() {
+    const { data } = useGetTransfers();
+
     return (
         <div className={styles.main}>
-            Transfers Page
+            <div className={styles.monthContainer}>
+                {data?.data.transfers.map((transfer) => {
+                    return (
+                        <div className={styles.transferRow}>
+                            <div className={styles.userDetails}>
+                                <div className={styles.pfp}>{getImageFromUser(transfer)}</div>
+                                <div className={styles.contentContainer}>
+                                    {transfer.destinationUser
+                                        ? <span className={styles.title}>{transfer.destinationUser.name}</span>
+                                        : <span className={styles.title}>{shortAddress(transfer.destinationAddress)}</span>}
+                                    {/* {transfer.destinationUser
+                                        ? <span className={styles.subtitle}>{transfer.destinationUser.email}</span>
+                                        : <span className={styles.subtitle}>{transfer.id}</span>} */}
+                                    <span className={styles.subtitle}>Status: {transfer.status}</span>
+                                </div>
+                            </div>
+                            <span className={styles.amount}>- {transfer.amount} USDC</span>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     );
 }
