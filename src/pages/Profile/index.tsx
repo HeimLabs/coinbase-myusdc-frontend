d7d2a24a75c357d31c9fdaab10eedb932156ceb9
Van Hohenheim <0xhohenheim@gmail.com>
2024-09-12 03:10:33 +0530

    feat: profile page

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,14 +1,47 @@
+import { qrIcon, walletIcon } from "../../assets";
 import { useAppUser } from "../../contexts/user.context";
+import { shortAddress } from "../../utils";
 import styles from "./Profile.module.scss";
 
 export default function Profile() {
-    const {signOut} = useAppUser();
+    const { user, signOut } = useAppUser();
 
     return (
         <div className={styles.main}>
-            Profile Page
-
-            <button onClick={signOut}>Sign Out</button>
+            {/* USER DETAILS */}
+            <div className={styles.userContainer}>
+                <div className={styles.userDetails}>
+                    <span className={styles.title}>{user?.name}</span>
+                    <span className={styles.subtitle}>{user?.email}</span>
+                    <span className={styles.subtitle}>{shortAddress(user?.wallet.address)}</span>
+                </div>
+                <div className={styles.pfpContainer}>
+                    <img className={styles.pfp} src={user?.imageUrl} alt="PFP" />
+                    <img className={styles.qr} src={qrIcon} alt="QR" />
+                </div>
+            </div>
+            {/* ACTIONS */}
+            <div className={styles.actionContainer}>
+                <div className={styles.actionRow}>
+                    <div className={styles.details}>
+                        <img src={walletIcon} alt="Wallet" />
+                        <div className={styles.content}>
+                            <span className={styles.balance}>${user?.wallet.usdcBalance}</span>
+                            <span className={styles.subtitle}>USDC Balance</span>
+                        </div>
+                    </div>
+                    <div className={styles.action}>
+                        <button className={`${styles.bttn} ${styles.dark}`}>
+                            Buy USDC
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <button className={`${styles.bttn} ${styles.light}`} onClick={signOut}>Sign Out</button>
+            <div className={styles.appDetails}>
+                <span className={styles.title}>MY USDC APP</span>
+                <span className={styles.subtitle}>Version 1.1.0</span>
+            </div>
         </div>
     );
 }
